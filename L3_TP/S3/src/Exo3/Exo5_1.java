ac0ba2324d3ea1251f69f502e95ec6b9f4848d62
chunhu16 <chg.hugo.chg@gmail.com>
2017-02-07 16:07:33 +0100

    Changement sur tp3

---
+++ L3_TP/S3/src/Exo3/Exo5_1.java
@@ -0,0 +1,15 @@
+package Exo3;
+
+
+public class Exo5_1 {
+
+	/**
+	 * Print the number of tries to get a snake eyes.
+	 * @param args Arguments.
+	 */
+	public static void main(String[] args) {
+		PairOfDice dices= new PairOfDice();
+		dices.snakeEyes();
+	}
+
+}
---
+++ L3_TP/S3/src/Exo7/IntQuestion.java
@@ -0,0 +1,15 @@
+package Exo7;
+
+
+public interface IntQuestion {
+	/**
+	 * Getter of the question.
+	 * @return The question.
+	 */
+	public String getQuestion();
+	/**
+	 * Getter of the correct answer.
+	 * @return The correct answer.
+	 */
+    public int getCorrectAnswer();
+}
